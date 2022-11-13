6076237482e1c6fde07cc8580bc3adf8ba7fd8a7
Gnik Droy <gnikdroy@gmail.com>
2022-11-13 04:23:01 +0000

    src: refactor sessions

--- lua/projections/session.lua
+++ lua/projections/session.lua
@@ -1,15 +1,17 @@
 local utils = require("projections.utils")
 local config = require("projections.config")
 local Workspace = require("projections.workspace")
+local Project = require("projections.project")
 
 local Session = {}
 Session.__index = Session
 
--- Returns the path of the session file
+-- Returns the path of the session file as well as project information
 -- Returns nil if path is not a valid project path
--- @returns nil | path of session file
-function Session.path(spath)
-    -- check if this is some project's session
+-- @args spath The path to project root
+-- @returns nil | path of session file and project information
+function Session.info(spath)
+    -- check if path is some project's root
     local path = Path.new(spath)
     local project_name = path:basename()
     local workspace_path = path:parent()
@@ -25,7 +27,10 @@ function Session.path(spath)
 
     local path_hash = utils._fnv1a(tostring(workspace_path))
     local filename = string.format("%s_%u.vim", project_name, path_hash)
-    return config.sessions_folder .. filename
+    return {
+        path = config.sessions_folder .. filename,
+        project = Project.new(project_name, workspace)
+    }
 end
 
 -- Ensures sessions folder is available
@@ -35,38 +40,74 @@ function Session._ensure_sessions_folder()
 end
 
 -- Attempts to store the session
+-- @args spath String representing the path to the project root
 -- @returns if operation was successful
 function Session.store(spath)
     Session._ensure_sessions_folder()
+    local session_info = Session.info(spath)
+    if session_info == nil then return false end
+    return Session.store_to_session_file(tostring(session_info.path))
+end
 
-    local session_path = Session.path(spath)
-    if session_path == nil then return false end
-
+-- Attempts to store to session file
+-- @args spath String representing the path to the session file
+-- @returns if operation was successful
+function Session.store_to_session_file(spath)
     if config.store_hooks.pre ~= nil then config.store_hooks.pre() end
     -- TODO: correctly indicate errors here!
-    vim.cmd("mksession! " .. tostring(session_path))
+    vim.cmd("mksession! " .. spath)
     if config.store_hooks.post ~= nil then config.store_hooks.post() end
     return true
 end
 
 -- Attempts to restore a session
+-- @args spath String representing the path to the project root
 -- @returns if operation was successful
 function Session.restore(spath)
     Session._ensure_sessions_folder()
-    print(string.format(":restoring %s", spath))
+    local session_info = Session.info(spath)
+    if session_info == nil or not session_info.path:is_file() then return false end
+    return Session.restore_from_session_file(tostring(session_info.path))
+end
 
-    local session_path = Session.path(spath)
-    if session_path == nil or not session_path:is_file() then return false end
-
+-- Attempts to restore a session from session file
+-- @args spath String representing path to session file
+-- @returns if operation was successful
+function Session.restore_from_session_file(spath)
     if config.restore_hooks.pre ~= nil then config.restore_hooks.pre() end
-    -- TODO: correctly indicate errors here!
     vim.cmd [[
         %bdelete!
         clearjumps
     ]]
-    vim.cmd("source " .. tostring(session_path))
+    -- TODO: correctly indicate errors here!
+    vim.cmd("source " .. spath)
     if config.restore_hooks.post ~= nil then config.restore_hooks.post() end
     return true
+end
+
+-- Get latest session
+-- @returns nil | path to latest session
+function Session.latest()
+    local latest_session = nil
+    local latest_timestamp = 0
+
+    for _, filename in ipairs(vim.fn.readdir(tostring(config.sessions_folder))) do
+        local session = config.sessions_folder .. filename
+        local timestamp = vim.fn.getftime(tostring(session))
+        if timestamp > latest_timestamp then
+            latest_session = session
+            latest_timestamp = timestamp
+        end
+    end
+    return latest_session
+end
+
+-- Restore latest session
+-- @returns if operation was successful
+function Session.restore_latest()
+    local latest_session = Session.latest()
+    if latest_session == nil then return false end
+    return Session.restore_from_session_file(tostring(latest_session))
 end
 
 return Session
